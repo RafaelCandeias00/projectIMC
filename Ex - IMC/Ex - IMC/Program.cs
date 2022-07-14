3d63538ef2974042ee4f6305328898e2c95833bc
Rafael Candeias <rafaelcandeias39@gmail.com>
2022-07-14 10:29:46 -0300

    Lista 2

--- Ex - IMC/Ex - IMC/Program.cs
+++ Ex - IMC/Ex - IMC/Program.cs
@@ -8,6 +8,10 @@ namespace Ex___IMC
         static void Main(string[] args)
         {
             Console.WriteLine("-- Índice de Massa Corporal (IMC) --");
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite sua idade: ");
+            string idade = Console.ReadLine();
             Console.Write("Digite sua altura: ");
             float altura = (float) Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.Write("Digite seu peso: ");
@@ -19,24 +23,24 @@ namespace Ex___IMC
 
             if(CalculoIMC >= 0 && CalculoIMC < 18.5)
             {
-                Console.WriteLine($"Seu IMC é: {CalculoIMC.ToString("F1", CultureInfo.InvariantCulture)} - Abaixo do peso");
+                Console.WriteLine($"Seu IMC é: {CalculoIMC.ToString("F1", CultureInfo.InvariantCulture)} - Abaixo do peso \nNome: {nome}\nIdade: {idade} anos");
             }
             if(CalculoIMC >= 18.5 && CalculoIMC < 24.9)
             {
-                Console.WriteLine($"Seu IMC é: {CalculoIMC.ToString("F1", CultureInfo.InvariantCulture)} - Normal");
+                Console.WriteLine($"Seu IMC é: {CalculoIMC.ToString("F1", CultureInfo.InvariantCulture)} - Normal \nNome: {nome}\nIdade: {idade} anos");
             }else if(CalculoIMC >= 24.9 && CalculoIMC < 29.9)
             {
-                Console.WriteLine($"Seu IMC é: {CalculoIMC.ToString("F1", CultureInfo.InvariantCulture)} - Sobrepeso");
+                Console.WriteLine($"Seu IMC é: {CalculoIMC.ToString("F1", CultureInfo.InvariantCulture)} - Sobrepeso \nNome: {nome}\nIdade: {idade} anos");
             }else if(CalculoIMC >= 30.0 && CalculoIMC < 35.9)
             {
-                Console.WriteLine($"Seu IMC é: {CalculoIMC.ToString("F1", CultureInfo.InvariantCulture)} - Obesidade Grau I");
+                Console.WriteLine($"Seu IMC é: {CalculoIMC.ToString("F1", CultureInfo.InvariantCulture)} - Obesidade Grau I \nNome: {nome}\nIdade: {idade} anos");
             }if (CalculoIMC >= 35.9 && CalculoIMC < 39.9) 
             {
-                Console.WriteLine($"Seu IMC é: {CalculoIMC.ToString("F1", CultureInfo.InvariantCulture)} - Obesidade Grau II");
+                Console.WriteLine($"Seu IMC é: {CalculoIMC.ToString("F1", CultureInfo.InvariantCulture)} - Obesidade Grau II \nNome: {nome}\nIdade: {idade} anos");
             }
             else if (CalculoIMC > 40)
             {
-                Console.WriteLine($"Seu IMC é: {CalculoIMC.ToString("F1", CultureInfo.InvariantCulture)} - Obesidade Grau III");
+                Console.WriteLine($"Seu IMC é: {CalculoIMC.ToString("F1", CultureInfo.InvariantCulture)} - Obesidade Grau III \nNome: {nome}\nIdade: {idade} anos");
             }
         }
     }
